68c14c2e72196b08316643d0126a87d8c86fca1a
Jack Maddigan <jack.g.maddigan@gmail.com>
2024-05-11 23:12:19 +1200

    fix error

--- src/megaRecords.js
+++ src/megaRecords.js
@@ -101,13 +101,13 @@ async function getFilteredRecords(recordsChannel) {
 async function createAndSendEmbed(record, recordsChannel) {
   const roleToPing = getRoleToPing[record.tag];
   console.log(record.result.attempts);
-  const timeList = `*(${centiToTime(
-    record.result.attempts[0].result
-  )}, ${centiToTime(record.result.attempts[1].result)}, ${centiToTime(
-    record.result.attempts[2].result
-  )}, ${centiToTime(record.result.attempts[3].result)}, ${centiToTime(
-    record.result.attempts[4].result
-  )})*`;
+  const timeList = `*(${
+    centiToTime(record.result.attempts[0].result) || "DNS"
+  }, ${centiToTime(record.result.attempts[1].result) || "DNS"}, ${
+    centiToTime(record.result.attempts[2].result) || "DNS"
+  }, ${centiToTime(record.result.attempts[3].result) || "DNS"}, ${
+    centiToTime(record.result.attempts[4].result) || "DNS"
+  })*`;
 
   const recordEmbed = new EmbedBuilder()
     .setColor(getColorOfTag[record.tag])
